7cbe3e2cb0939f806bb56696c4ef50d78e1e6e73
XiangYe <xiangyegit@foxmail.com>
2022-05-06 14:49:24 +0800

    init commit

---
+++ LeetCode/1.两数之和.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  return [];
+}
+// @lc code=end
