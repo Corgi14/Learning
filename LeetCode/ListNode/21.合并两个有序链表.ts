fe23e457bb951f19a8ea37a7db11cda453fed160
XiangYe <xiangyegit@foxmail.com>
2022-05-17 22:02:54 +0800

    LeetCode 21
    2022.5.17 二刷 没记住

--- LeetCode/ListNode/21.合并两个有序链表.ts
+++ LeetCode/ListNode/21.合并两个有序链表.ts
@@ -13,24 +13,23 @@ function mergeTwoLists(
 ): ListNode | null {
   let dummy = new ListNode(-1);
   let p = dummy;
-  let p1 = list1,
-    p2 = list2;
-  while (p1 && p2) {
-    if (p1.val < p2.val) {
-      p.next = p1;
-      p1 = p1.next;
+  while (list1 !== null && list2 !== null) {
+    if (list1.val > list2.val) {
+      p.next = list2;
+      list2 = list2.next;
     } else {
-      p.next = p2;
-      p2 = p2.next;
+      p.next = list1;
+      list1 = list1.next;
     }
     p = p.next;
   }
-  if (p1) {
-    p.next = p1;
+  if (list1.next !== null) {
+    p.next = list1;
   }
-  if (p2) {
-    p.next = p2;
+  if (list2.next !== null) {
+    p.next = list2;
   }
   return dummy.next;
 }
 // @lc code=end
+//2022.5.17 二刷 没记住
