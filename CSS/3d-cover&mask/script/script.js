4a9777d2701d49e91cfc7cfa86c7ac4e587eab86
XiangYe <xiangyegit@foxmail.com>
2022-06-01 09:19:27 +0800

    update overlay logic

--- CSS/3d-cover&mask/script/script.js
+++ CSS/3d-cover&mask/script/script.js
@@ -1,6 +1,7 @@
 const ACTIVE = "active";
 
 const active_img = document.querySelectorAll(".main-structure .content img");
+const video_overlay = document.querySelector(".main-structure .content .video-overlay");
 
 const menu_logo = document.querySelector("header .menu-logo");
 const main_header = document.querySelector(".content .text-description h1");
@@ -29,6 +30,37 @@ menu_button.forEach((button, index) => {
       video.classList.remove(ACTIVE);
     });
     active_img[index].classList.add(ACTIVE);
-    console.log(index);
+    switchContent(index);
   });
 });
+
+const switchContent = (index) => {
+  switch (index) {
+    case 0:
+      main_header.innerHTML = CONTENT_INFO.skate_board.h1;
+      second_header.innerHTML = CONTENT_INFO.skate_board.h2;
+      paragraph.innerHTML = CONTENT_INFO.skate_board.p;
+      video_overlay.className = "video-overlay video-overlay-skate";
+      break;
+    case 1:
+      main_header.innerHTML = CONTENT_INFO.winner.h1;
+      second_header.innerHTML = CONTENT_INFO.winner.h2;
+      paragraph.innerHTML = CONTENT_INFO.winner.p;
+      video_overlay.className = "video-overlay video-overlay-winner";
+      break;
+    case 2:
+      main_header.innerHTML = CONTENT_INFO.painter.h1;
+      second_header.innerHTML = CONTENT_INFO.painter.h2;
+      paragraph.innerHTML = CONTENT_INFO.painter.p;
+      video_overlay.className = "video-overlay video-overlay-painter";
+      break;
+    case 3:
+      main_header.innerHTML = CONTENT_INFO.farmer.h1;
+      second_header.innerHTML = CONTENT_INFO.farmer.h2;
+      paragraph.innerHTML = CONTENT_INFO.farmer.p;
+      break;
+      video_overlay.className = "video-overlay video-overlay-farmer";
+    default:
+      break;
+  }
+};
