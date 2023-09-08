1bd277484a16c8a88c71b5a5bb0cc180f42aa1d1
XiangYe <xiangyegit@foxmail.com>
2023-09-08 16:11:36 +0800

    update

---
+++ LeetCode/Array/151.反转字符串中的单词.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=typescript
+ *
+ * [151] 反转字符串中的单词
+ */
+
+// @lc code=start
+function reverseWords(s: string): string {
+    return s.trim().split("/\s+/").reverse().join(" ")
+};
+// @lc code=end
+
